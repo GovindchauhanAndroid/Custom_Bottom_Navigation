2caddbd638360d1243ac2850c52b37124ae17ef0
govindsingh <chauhangovind69@gmail.com>
2021-08-23 12:33:08 +0530

    all update in bottom bar in android

--- app/src/main/java/com/mytechcasa/custombottomnavigationbar/MainActivity.java
+++ app/src/main/java/com/mytechcasa/custombottomnavigationbar/MainActivity.java
@@ -1,8 +1,5 @@
 package com.mytechcasa.custombottomnavigationbar;
 
-import android.annotation.SuppressLint;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,6 +8,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,73 +16,58 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     BottomNavigationView bottomNavigationView;
     private int mMenuId;
-    FloatingActionButton fltBtn;
+    Fragment fragment = null;
+    FloatingActionButton floatingActionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView = findViewById(R.id.navigation_view);
+        bottomNavigationView = findViewById(R.id.navigationBar);
         bottomNavigationView.setBackground(null);
-        bottomNavigationView.setSelectedItemId(R.id.home);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        fltBtn = findViewById(R.id.floatingBtn);
-        Toast.makeText(MainActivity.this, "OnCreate", Toast.LENGTH_LONG).show();
-        fltBtn.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        floatingActionButton = findViewById(R.id.floating_btn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Chat.class));
-                Toast.makeText(MainActivity.this, "floating button", Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        mMenuId = item.getItemId();
-        for (int i = 0; i < bottomNavigationView.getMenu().size(); i++) {
-            MenuItem menuItem = bottomNavigationView.getMenu().getItem(i);
-            boolean isChecked = menuItem.getItemId() == item.getItemId();
-            menuItem.setChecked(isChecked);
-
-        }
-
         switch (item.getItemId()) {
+            case R.id.home:
+                fragment = new profile_Fragment();
+                LoadFragment();
+                break;
             case R.id.chat:
                 startActivity(new Intent(MainActivity.this, Chat.class));
-                Toast.makeText(getApplicationContext(), "chat", Toast.LENGTH_LONG).show();
                 break;
 
             case R.id.sell:
                 Toast.makeText(getApplicationContext(), "sell", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(MainActivity.this, Chat.class));
                 break;
             case R.id.show_ads:
-                Toast.makeText(getApplicationContext(), "show_ads", Toast.LENGTH_LONG).show();
                 startActivity(new Intent(MainActivity.this, Chat.class));
                 break;
             case R.id.account:
-                Fragment fragment = new Fragment();
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                transaction.replace(R.id.fram_layput, fragment); // give your fragment container id in first parameter
-               transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
-                transaction.commit();
+                fragment = new ProfileFragment();
+                LoadFragment();
         }
-
-
         return true;
     }
 
-    @Override
-    public void onBackPressed() {
-        bottomNavigationView.setSelectedItemId(R.id.home);
-        super.onBackPressed();
+    void LoadFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment).commit();
     }
+
 
     @Override
     protected void onStart() {
         bottomNavigationView.getMenu().findItem(R.id.home).setChecked(true);
-        Toast.makeText(getApplicationContext(), "start", Toast.LENGTH_LONG).show();
         super.onStart();
     }
 }
